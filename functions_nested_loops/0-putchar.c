94d27ff6bbeab04e45d4a055011ab003ab97fdf7
MINS2405 <9963@holbertonstudents.com>
2024-10-25 12:37:37 +0000

    pwd

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,3 +1,5 @@
+#inculde <stdio.h>
+#inculde <unistd.h>
 #include 'main.h'
 
 /**
@@ -10,7 +12,7 @@
 
 int main(void) 
 {
-	_putchar('_')
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
