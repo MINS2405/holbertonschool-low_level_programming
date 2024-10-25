64ffc2431bc6d849fab662c42da8cd6f5dcc438a
MINS2405 <9963@holbertonstudents.com>
2024-10-25 12:02:08 +0000

    pwd

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main.h
+{
+	use putchar;
+
+	return (0);
+}
