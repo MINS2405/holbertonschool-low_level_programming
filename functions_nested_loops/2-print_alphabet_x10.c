2c6c28398692ad3967de3c27816e1b2b805202b8
MINS2405 <9963@holbertonstudents.com>
2024-10-25 19:15:49 +0000

    pwd

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Entry point of program
+ * print_alphabet_x10 - Entry point of the program
  *
- * description: Prints a quote using the puts function
- * Return: always 0 (Success)
+ * Description: Prints a quote using the puts function
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
