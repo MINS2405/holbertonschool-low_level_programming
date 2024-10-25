af9a2484dc80a22e7721b6ecaa793a589f673bae
MINS2405 <9963@holbertonstudents.com>
2024-10-25 12:15:21 +0000

    pwd

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -7,10 +6,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
-int 
+int _putchar(char c)
 {
-	gcc(use putchar);
-
-	return (0);
+	return (write(1, &c, 1));
 }
