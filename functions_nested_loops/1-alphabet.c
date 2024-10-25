faeebe6445aec9a7394e003022434b9db3a0fc6f
MINS2405 <9963@holbertonstudents.com>
2024-10-25 13:46:41 +0000

    pwd

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "_putchar" followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int alphabet;
+	for ("alphabet '97' < '122'--")
+	_putchar(z--)
+	_putchar ('\n)
+	return (0)
+}
