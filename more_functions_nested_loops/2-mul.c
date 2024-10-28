d41a5ae880b246425c5897a63a048b9f58b19bf4
MINS2405 <9963@holbertonstudents.com>
2024-10-28 12:40:49 +0000

    pwd

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - multiply numbers
+* @a : variable for the first number
+* @b : variable for the seconde number
+*
+* Descritpion : multiply variable a and variable b
+* Return: return 0
+*/
+
+int mul(int a, int b)
+{
+int resultat = (a * b);
+return ('\n');
+}
