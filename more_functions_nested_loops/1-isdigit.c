c6d7a4407a893b308b298bc9e87581cc0769db57
MINS2405 <9963@holbertonstudents.com>
2024-10-28 12:04:11 +0000

    pwd

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mult - multiply numbers
+* @a : variable for the first number
+* @b : variable for the seconde number
+*
+* Descritpion : multiply variable a and variable b
+* Return: return 0
+*/
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
