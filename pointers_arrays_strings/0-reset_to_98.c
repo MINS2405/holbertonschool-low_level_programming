2ab481f730d9a1fb7d025cc3d6cdf499b243d827
MINS2405 <9963@holbertonstudents.com>
2024-10-30 13:32:14 +0000

    pwd

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 
+ *
+ * Description:  function that takes a pointer to an int as parameter a
+ *
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
