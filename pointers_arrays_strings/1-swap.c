e34a77aef2bcd2d95c39754bafddcb89d35b0c2b
MINS2405 <9963@holbertonstudents.com>
2024-10-30 13:53:53 +0000

    pwd

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * user the prototypes: void swap_int to int *a, int *b
+ *
+ * Description: function that swaps the values of two integers
+ *
+ */
+void swap_int(int *a, int *b)
+{
+int i;
+
+i = *a;
+*a = *b;
+*b = *i;
+}
