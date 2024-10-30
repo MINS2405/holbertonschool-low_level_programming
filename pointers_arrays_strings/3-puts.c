6b751462eb826a8a608f7757d3cd8a74fe2b97d1
MINS2405 <9963@holbertonstudents.com>
2024-10-30 14:50:22 +0000

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#inculde "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: Pointer to the string to be printed
+ *
+ * Description: This function takes a pointer to a null-terminated string
+ * and prints each character of the st
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
