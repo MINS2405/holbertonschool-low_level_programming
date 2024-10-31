b88ff1104d3a7c52470bc880fe21d0f72dd1e16f
MISN2405 <https://mail.holbertonstudents.com>
2024-10-31 12:28:52 +0000

    pwd

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Function: print_rev
+ * Prototype: void print_rev(char *s)
+ *
+ * Description: This function prints a given string in reverse order,
+ * followed by a new line. It takes a single parameter
+ *
+ * Return: always
+ */
+
+void print_rev(char *s)
+
+int i = 0;
+{
+while (*s != '\0')
+{
+s++;
+i--;
+}
+return ('\n')
+}
