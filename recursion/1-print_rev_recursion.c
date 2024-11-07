708a5618afb3185d963ca4620c859fb7b469ddff
MINS2405 <9963@holbertonstudents.com>
2024-11-07 20:33:42 +0000

    pwd

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _print_rev_recursion - Imprime une chaîne en sens inverse
+* @s: La chaîne à imprimer
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
