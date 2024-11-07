0e2c8112f4d55a48cb75a6949b304a1a94148dcf
MINS2405 <9963@holbertonstudents.com>
2024-11-07 20:50:44 +0000

    pwd

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _pow_recursion - Calcule x élevé à la puissance y
+* @x: La base
+* @y: L'exposant
+*
+* Return: x^y, ou -1 si y est négatif
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
