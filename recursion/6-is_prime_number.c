5d653a674c92c6cebb5a8e501ffa5bf157a37f73
MINS2405 <9963@holbertonstudents.com>
2024-11-07 21:04:07 +0000

    pwd

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* check_prime - Fonction auxiliaire pour vérifier si un nombre est premier
+* @n: Le nombre à vérifier
+* @i: Le diviseur actuel à tester
+*
+* Return: 1 si n est premier, 0 sinon
+*/
+int check_prime(int n, int i)
+{
+if (i * i > n)
+return (1);
+if (n % i == 0)
+return (0);
+return (check_prime(n, i + 1));
+}
+/**
+* is_prime_number - Détermine si un nombre est premier
+* @n: Le nombre à vérifier
+*
+* Return: 1 si n est premier, 0 sinon
+*/
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+return (check_prime(n, 2));
+}
