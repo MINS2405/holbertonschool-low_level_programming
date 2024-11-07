2311d77642c203d2d035aec0644efb82bf4e3eb1
MINS2405 <9963@holbertonstudents.com>
2024-11-07 14:25:55 +0000

    pwd

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts_recursion - Imprime chaîne suivie d'une nouvelle lignerécursivemen
+* @s: La chaîne à imprimer
+*/
+void _puts_recursion(char *s)
+{
+
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+
+_puts_recursion(s + 1);
+}
