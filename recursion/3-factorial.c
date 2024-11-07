3e6e4a3e9c36535c8d1c5ae1329f1dbae8320b66
MINS2405 <9963@holbertonstudents.com>
2024-11-07 20:44:46 +0000

    pwd

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - Calcule le factoriel d'un nombre donné
+ * @n: Le nombre dont on veut calculer le factoriel
+ *
+ * Return: Le factoriel de n, ou -1 si n est négatif
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+return (1);
+}
+return (n * factorial(n - 1));
+}
