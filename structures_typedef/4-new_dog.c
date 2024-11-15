b18936e28b4196e38b3e6095a29e79a680d34d0e
MINS2405 <9963@holbertonstudents.com>
2024-11-15 15:24:37 +0000

    pwd

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,62 +3,73 @@
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * _strlen - multiply numbers
+ * @str : variable for the first number
  *
- * Return: Pointer to the new dog (Success), NULL otherwise
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+int _strlen(char *str)
+{
+int length = 0;
+while (str[length] != '\0')
+length++;
+return (length);
+}
+
+/**
+ * _strcpy - multiply numbers
+ * @dest : variable for the first number
+ * @src : variable for the seconde number
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
+
+/**
+ * new_dog - multiply numbers
+ * @name : variable for the first number
+ * @age : variable for the seconde number
+ * @owner : betty
+ *
+ * Descritpion : multiply variable a and variable b
+ * Return: return 0
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
- dog_t *dog;
-int i, j;
+dog_t *dog;
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
 return (NULL);
-if (name != NULL) 
-{
-for (i = 0; name[i] != '\0'; i++);
-dog->name = malloc(i + 1);
-if (dog->name == NULL) 
+dog->name = malloc(_strlen(name) + 1);
+if (dog->name == NULL)
 {
 free(dog);
 return (NULL);
 }
-for (j = 0; j < i; j++) 
-{
-dog->name[j] = name[j];
-}
-dog->name[j] = '\0';
-} 
-else 
-{
-dog->name = NULL;
-}
-if (owner != NULL) 
-{
-for (i = 0; owner[i] != '\0'; i++);
-dog->owner = malloc(i + 1);
-if (dog->owner == NULL) 
+_strcpy(dog->name, name);
+dog->owner = malloc(_strlen(owner) + 1);
+if (dog->owner == NULL)
 {
 free(dog->name);
 free(dog);
 return (NULL);
 }
-for (j = 0; j < i; j++) 
-{
-dog->owner[j] = owner[j];
-}
-dog->owner[j] = '\0';
-} 
-else 
-{
-dog->owner = NULL;
-}
-
+_strcpy(dog->owner, owner);
 dog->age = age;
-
 return (dog);
 }
