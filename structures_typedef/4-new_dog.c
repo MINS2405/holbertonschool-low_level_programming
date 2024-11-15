40a99b221b33695f0f4c8367c210b5aa27b64f58
MINS2405 <9963@holbertonstudents.com>
2024-11-15 13:55:58 +0000

    pwd

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the new dog (Success), NULL otherwise
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *dog;
+int len_name, len_owner;
+
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+
+len_name = strlen(name);
+len_owner = strlen(owner);
+
+dog->name = malloc(sizeof(char) * (len_name + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+strcpy(dog->name, name);
+
+dog->owner = malloc(sizeof(char) * (len_owner + 1));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+strcpy(dog->owner, owner);
+
+dog->age = age;
+
+return (dog);
+}
