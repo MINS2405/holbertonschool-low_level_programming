53c468a9b3cfecb369f88c4e22cdf52405227cf1
MINS2405 <9963@holbertonstudents.com>
2024-11-15 15:16:52 +0000

    pwd

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,32 +13,50 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog;
-int len_name, len_owner;
-
+ dog_t *dog;
+int i, j;
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
 return (NULL);
-
-len_name = strlen(name);
-len_owner = strlen(owner);
-
-dog->name = malloc(sizeof(char) * (len_name + 1));
-if (dog->name == NULL)
+if (name != NULL) 
+{
+for (i = 0; name[i] != '\0'; i++);
+dog->name = malloc(i + 1);
+if (dog->name == NULL) 
 {
 free(dog);
 return (NULL);
 }
-strcpy(dog->name, name);
-
-dog->owner = malloc(sizeof(char) * (len_owner + 1));
-if (dog->owner == NULL)
+for (j = 0; j < i; j++) 
+{
+dog->name[j] = name[j];
+}
+dog->name[j] = '\0';
+} 
+else 
+{
+dog->name = NULL;
+}
+if (owner != NULL) 
+{
+for (i = 0; owner[i] != '\0'; i++);
+dog->owner = malloc(i + 1);
+if (dog->owner == NULL) 
 {
 free(dog->name);
 free(dog);
 return (NULL);
 }
-strcpy(dog->owner, owner);
+for (j = 0; j < i; j++) 
+{
+dog->owner[j] = owner[j];
+}
+dog->owner[j] = '\0';
+} 
+else 
+{
+dog->owner = NULL;
+}
 
 dog->age = age;
 
